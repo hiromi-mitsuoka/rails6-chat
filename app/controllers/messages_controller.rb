2cdaa59700c51b1356b827e9440f834a43a7d7bd
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2021-01-06 03:56:29 +0000

    Finish delete message in all room

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,17 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  
   def create
     @message = current_user.messages.create!(message_params)
     # 投稿されたメッセージを全員に配信
     # template : message.rbに記述
     ActionCable.server.broadcast "room_channel", message: @message.template
+  end
+  
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to rooms_show_path
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # 全体トークの無限スクロール対応用のルーティング
   get '/show_additionally', to: 'rooms#show_additionally'
   mount ActionCable.server => '/cable'
-  resources :messages, only: :create
+  resources :messages, only: [:create, :destroy]
   get 'rooms/show'
   devise_for :users
   resources :users, :only => [:index, :show]
