37c3bbe463e0f2a205d5fcf0ec0dd7451a677cd3
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2021-01-08 12:01:30 +0000

    Add clmtrackr but comment out

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("custom")
 
 // bootstrap
 require("bootstrap/dist/js/bootstrap")
 // jquery
 require('jquery')
+
+// import 'bootstrap'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
---
+++ app/javascript/packs/custom/application.js
@@ -0,0 +1,5 @@
+require('custom/clmtrackr')
+require('custom/model_pca_20_svm_emotion')
+require('custom/emotionClassifier')
+require('custom/emotionModel')
+require('custom/face_tracker')
