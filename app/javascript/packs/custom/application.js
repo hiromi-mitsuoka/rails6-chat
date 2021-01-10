43c69fc70008f262ef05147fe8c626c795951b48
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2021-01-10 02:15:44 +0000

    Plus clmtracker.min

--- app/javascript/packs/custom/application.js
+++ app/javascript/packs/custom/application.js
@@ -1,5 +1,7 @@
-require('custom/clmtrackr')
-require('custom/model_pca_20_svm_emotion')
-require('custom/emotionClassifier')
-require('custom/emotionModel')
-require('custom/face_tracker')
+// require('custom/clmtrackr')
+// require('custom/model_pca_20_svm_emotion')
+// require('custom/emotionClassifier')
+// require('custom/emotionModel')
+// require('custom/face_tracker')
+
+require('custom/clmtrackr.min')
