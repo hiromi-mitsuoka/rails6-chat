186079fd36f4f53c373829835af554747e6d8b9a
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2021-01-11 15:17:41 +0000

    reverse redirect_to broadcast

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,11 @@ class MessagesController < ApplicationController
   def create
     @message = current_user.messages.create!(message_params)
     @message_user = current_user
-    # 投稿されたメッセージを全員に配信
-    
-    # ActionCable動かなくなったから一旦これで
-    # template : message.rbに記述
-    ActionCable.server.broadcast "room_channel", message: @message.template
+
     # current_userを適応させるため
     redirect_to rooms_show_path
+    # template : message.rbに記述
+    ActionCable.server.broadcast "room_channel", message: @message.template
   end
   
   def destroy
