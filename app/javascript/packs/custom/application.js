07eff6dfc7e3dbb715ceda6c40524dd31c380c19
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2021-01-14 09:33:57 +0000

    Remove clmtracker

--- app/javascript/packs/custom/application.js
+++
@@ -1,7 +0,0 @@
-// require('custom/clmtrackr')
-// require('custom/model_pca_20_svm_emotion')
-// require('custom/emotionClassifier')
-// require('custom/emotionModel')
-// require('custom/face_tracker')
-
-require('custom/clmtrackr.min')
